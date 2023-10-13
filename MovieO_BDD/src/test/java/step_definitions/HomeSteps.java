8b615cf4a499b009a5a22d11776e1ae29711a9b0
juliaumar <57296716+juliaumar@users.noreply.github.com>
2023-10-13 14:22:33 -0400

    Add files via upload

---
+++ MovieO_BDD/src/test/java/step_definitions/HomeSteps.java
@@ -0,0 +1,36 @@
+package step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import pages.CommonPage;
+import pages.HomePage;
+import utils.BrowserUtils;
+
+public class HomeSteps implements CommonPage {
+    HomePage page;
+
+    public HomeSteps() {
+        page = new HomePage();
+
+    }
+    @Given("I open url of homepage")
+    public void i_open_url_of_homepage() {
+        BrowserUtils.getDriver();
+
+
+
+
+    }
+
+    @Then("Verify Title of Home page is {string}")
+    public void verifyTitleOfHomePageIs(String title) {
+        Assert.assertEquals(title, BrowserUtils.getDriver().getTitle());
+    }
+    }
---
+++ MovieO_BDD/src/test/resources/features/home.feature
@@ -0,0 +1,9 @@
+@homeScenarios
+Feature: Home Page Related Scenarios
+
+  Background: Navigate to page
+    Given I open url of homepage
+
+    @M-01
+    Scenario: Verify the Title of the page
+      Then Verify Title of Home page is "Movieo"
